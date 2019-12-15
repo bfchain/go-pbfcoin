f99181a58207442f047135202d47727f4ce7e3a9
Wenger Wu <124599@qq.com>
2019-12-15 11:02:52 -0500

    Add files via upload

---
+++ pow/block.go
@@ -0,0 +1,37 @@
+// Copyright 2014 The go-pbfcoin Authors
+// This file is part of the go-pbfcoin library.
+//
+// The go-pbfcoin library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-pbfcoin library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-pbfcoin library. If not, see <http://www.gnu.org/licenses/>.
+
+package pow
+
+import (
+	"math/big"
+
+	"github.com/pbfcoin/go-pbfcoin/common"
+	"github.com/pbfcoin/go-pbfcoin/core/types"
+)
+
+type Block interface {
+	Difficulty() *big.Int
+	HashNoNonce() common.Hash
+	Nonce() uint64
+	MixDigest() common.Hash
+	NumberU64() uint64
+}
+
+type ChainManager interface {
+	GetBlockByNumber(uint64) *types.Block
+	CurrentBlock() *types.Block
+}
